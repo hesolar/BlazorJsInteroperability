f310fed666abd81c01679151418339bd54dcd4e7
Héctor Solar <hectorsolar99@gmail.com>
2021-11-09 12:51:29 +0100

    5º grafico

---
+++ wwwroot/js/GoogleCharts.js
@@ -0,0 +1,41 @@
+﻿function googleCharts() {
+    google.charts.load('current', { packages: ['corechart', 'line'] });
+    google.charts.setOnLoadCallback(initChart);
+
+    function initChart() {
+        var xs = [1,2,3,4,5];
+        var ys = [5,10,15,20,25];
+        window.createNewChart({ xs, ys });
+    }
+
+    window.createNewChart = (params) => {
+        var xs = params.xs;
+        var ys = params.ys;
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('number', 'X');
+        data.addColumn('number', 'Y');
+
+        for (var i = 0; i < ys.length; i++) {
+            data.addRow([xs[i], ys[i]]);
+        }
+
+        var options = {
+            hAxis: { title: 'Horizontal Axis Label' },
+            vAxis: { title: 'Vertical Axis Label' },
+            title: 'This is a Google Chart in Blazor',
+            legend: { position: 'none' },
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+    };
+
+
+
+
+}
+
+//<script src="js/bootstrap.bundle.min.js"></script>
+//    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
